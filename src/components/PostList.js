19da3f0c361a2ebabc64e82e890e016acb15dfd1
dung p <pdungv101@gmail.com>
2024-12-04 20:18:47 +0700

    test 2

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,9 @@
 import Post from "./Post";
 
 const PostList = ({ posts, onUpdate, onDelete }) => {
+  if (!posts || posts.length === 0) {
+    return <p>No posts available.</p>;
+  }
   return (
     <div className="max-w-2xl mx-auto p-4">
       {posts.map((post) => (
